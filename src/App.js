d8062433cd0725d924ba428c5d3a4a261f2cef4b
cristoferprago <cristoferprago0904@gmail.com>
2023-01-27 15:24:42 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Scarecrow from './Assets/Images/Scarecrow.png';
+
+
+<link href='https://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet'></link>
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="div-container">
+      <h2 className="title">404 NOT FOUND</h2>
+      <div className='container'>
+        <div className="image-container">
+          <img src={Scarecrow} alt="Scarecrow" className='image'/>
+        </div>
+        <div className="text-container">
+          <span className='span-title'>I have bad news for you</span>
+          <span className='span-body'>The page you are looking for might be removed or is temporarily unavailable</span>
+          <button className='button'>BACK TO HOMEPAGE</button>
+        </div>
+      </div>
     </div>
   );
 }
